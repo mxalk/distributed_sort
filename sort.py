cda723db9027c2047567a7be02a7a4a69c0dde2b
S.Lu <s.lu@SLudeMacBook-Pro.local>
2020-11-19 22:52:19 +0100

    add radixsort

--- sort.py
+++ sort.py
@@ -1,7 +1,7 @@
 import sys
 
-Input_File = 'three'
-Output_File = 'Output_gensortDataFile'
+Input_File = 'gensortDataFile'
+Output_File = 'Output_gensortDataFile_rs'
 
 def insertion_sort(list):
     for i in range(1, len(list)):
@@ -12,6 +12,27 @@ def insertion_sort(list):
                 position = position - 1
         list[position] = current_value
 
+
+def radix_sort(list):
+    char_list = [chr(x) for x in range(32,127)]
+    max_length = 4 ### compare first 4 characters
+
+    for i in range(max_length):
+        bucket = {}
+        for char in char_list:
+            bucket[ord(char)] = []
+        for tuple in list:
+            temp_word = tuple[0:4]
+            bucket[ord(temp_word[max_length - i - 1])].append(tuple)
+ 
+        index = 0
+        for char in char_list:
+            if (len(bucket[ord(char)])) != 0:
+                for i in bucket[ord(char)]:
+                    list[index] = i
+                    index = index + 1
+        
+
 if __name__ == "__main__":
     try:    
         with open(Input_File,'r') as raw_input:
@@ -20,8 +41,8 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print('Cannot Open The File!')
 
-    insertion_sort(tuple) 
-
+    #insertion_sort(tuple) 
+    radix_sort(tuple)
     # for i in range(len(tuple)) :
     #     print(ord(tuple[i][0]),end="--")
     #     print(ord(tuple[i][1]))
